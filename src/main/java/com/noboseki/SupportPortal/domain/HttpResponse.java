1cf084edee2f1fdbff3cc2568e849c13480dc14a
Ragnar <wit.dawid@interia.pl>
2021-02-06 15:45:06 +0100

    first commit

---
+++ src/main/java/com/noboseki/SupportPortal/domain/HttpResponse.java
@@ -0,0 +1,18 @@
+package com.noboseki.SupportPortal.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HttpResponse {
+    private int httpStatusCode;
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+}
