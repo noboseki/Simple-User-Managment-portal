60fe2e4348a1243005792ff478f08510359329ca
Ragnar <wit.dawid@interia.pl>
2021-02-09 19:46:37 +0100

    Create ExceptionHandling.java and custom exceptions

---
+++ src/main/java/com/noboseki/supportportal/exception/domain/EmailExistException.java
@@ -0,0 +1,8 @@
+package com.noboseki.supportportal.exception.domain;
+
+public class EmailExistException extends Exception {
+
+    public EmailExistException(String message) {
+        super(message);
+    }
+}
