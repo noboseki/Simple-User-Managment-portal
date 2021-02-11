d4b65b52fa88a0c82df389e855e613c98ca8cd36
DawidW1993 <wit.dawid@interia.pl>
2021-02-11 20:46:22 +0100

    Create LoginAttemptService.java and Listeners.

---
+++ src/main/java/com/noboseki/supportportal/listener/AuthenticationSuccessListener.java
@@ -0,0 +1,23 @@
+package com.noboseki.supportportal.listener;
+
+import com.noboseki.supportportal.domain.User;
+import com.noboseki.supportportal.service.LoginAttemptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationSuccessListener {
+    private final LoginAttemptService loginAttemptService;
+
+    @EventListener
+    public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof User) {
+            User user = (User) event.getAuthentication().getPrincipal();
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+        }
+    }
+}
