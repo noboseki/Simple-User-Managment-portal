7133a6b3c51e45ce93392861ba0208b9c5bb688c
DawidW1993 <wit.dawid@interia.pl>
2021-02-18 15:15:03 +0100

    Implement methods into UserController

--- src/main/java/com/noboseki/supportportal/controller/UserController.java
+++ src/main/java/com/noboseki/supportportal/controller/UserController.java
@@ -18,13 +18,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static com.noboseki.supportportal.constant.FileConstant.FORWARD_SLASH;
-import static com.noboseki.supportportal.constant.FileConstant.USER_FOLDER;
+import static com.noboseki.supportportal.constant.FileConstant.*;
 import static com.noboseki.supportportal.constant.SecurityConstant.JWT_TOKEN_HEADER;
 import static com.noboseki.supportportal.constant.UserServiceImplConstant.USER_WAS_FOUND;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
@@ -136,6 +138,20 @@ public class UserController extends ExceptionHandling {
     @GetMapping(path = "/image/{username}/{fileName}", produces = IMAGE_JPEG_VALUE)
     public byte[] getProfileImage(@PathVariable("username") String username, @PathVariable("fileName") String fileName) throws IOException {
         return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + fileName));
+    }
+
+    @GetMapping(path = "/image/profile/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte [] chunk = new byte[1024];
+            while ((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+        }
+        return byteArrayOutputStream.toByteArray();
     }
 
     private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
